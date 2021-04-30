3112370521774678c4fa97fb1dae58bb0ae14fbb
Abderrahim Mayaba <ammayaba@gmail.com>
2021-05-01 02:44:48 +0300

    all reviews are extracted

--- steam_crawler/spiders/reviews.py
+++ steam_crawler/spiders/reviews.py
@@ -5,12 +5,23 @@ from ..items import GameReview
 class ReviewsSpider(scrapy.Spider):
     name = 'reviews'
     allowed_domains = ['steamcommunity.com']
-    start_urls = ['http://steamcommunity.com/']
-    appids = '1189690'
+    start_urls = [
+        'http://steamcommunity.com/app/316790/reviews/?browsefilter=mostrecent&p=1&filterLanguage=english']
 
     def parse(self, response):
-        pass
+        # get all reviews
+        all_reviews = response.css("div .apphub_Card")
 
+        # add enumeration if you need the order of the review
+        for r in all_reviews:
+            ur = GameReview()
+            # get review data
+            ur['rate'] = r.css(".title::text").extract_first()
+            ur['date'] = r.css(".date_posted::text").extract_first().split(
+                ':')[-1].strip()
+            ur['hours'] = float(r.css(
+                ".hours::text").extract_first().split(' ')[0].strip())
+            ur['text'] = ''.join(
+                r.css(".apphub_CardTextContent::text").extract()).strip()
 
-    def getReviews(self, response):
-        pass
+            yield ur
