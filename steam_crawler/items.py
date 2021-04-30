d97ef8943ee97deec913ab30b84c5a01e5e18fb6
Abderrahim Mayaba <ammayaba@gmail.com>
2021-04-30 08:42:11 +0300

    adding basic info item

--- steam_crawler/items.py
+++ steam_crawler/items.py
@@ -1,12 +1,17 @@
 # Define here the models for your scraped items
 #
-# See documentation in:
-# https://docs.scrapy.org/en/latest/topics/items.html
+# See documentation in=
+# https=//docs.scrapy.org/en/latest/topics/items.html
 
 import scrapy
 
 
-class SteamCrawlerItem(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+class GameBasicInfo(scrapy.Item):
+    # define the fields for your item here like=
+    appid = scrapy.Field()
+    title = scrapy.Field()
+    developer = scrapy.Field()
+    publisher = scrapy.Field()
+    tags = scrapy.Field()
+    genres = scrapy.Field()
+    early_access = scrapy.Field()
--- steam_crawler/spiders/games.py
+++ steam_crawler/spiders/games.py
@@ -1,4 +1,5 @@
 import scrapy
+from ..items import GameBasicInfo
 
 
 class GamesSpider(scrapy.Spider):
@@ -22,25 +23,29 @@ class GamesSpider(scrapy.Spider):
 
     # callback function for game url
     def parse_game(self, response):
-        title = response.xpath(
+        # create Basic Information item
+        bi = GameBasicInfo()
+
+        bi['appid'] = response.request.url.split('/')[-3]
+        bi['title'] = response.xpath(
             '/html/body/div[1]/div[7]/div[4]/div[1]/div[3]/div[2]/div[2]/div/div[3]/text()').extract_first()
-        developer = response.xpath(
+        bi['developer'] = response.xpath(
             '/html/body/div[1]/div[7]/div[4]/div[1]/div[3]/div[4]/div[1]/div/div[1]/div/div[3]/div/div[4]/div[2]/a/text()').extract()
-        publisher = response.xpath(
+        bi['publisher'] = response.xpath(
             '/html/body/div[1]/div[7]/div[4]/div[1]/div[3]/div[4]/div[1]/div/div[1]/div/div[3]/div/div[4]/div[2]/a/text()').extract()
-        url = response.request.url
         tags = response.xpath(
             '/html/body/div[1]/div[7]/div[4]/div[1]/div[3]/div[4]/div[1]/div/div[1]/div/div[4]/div/div[2]/a/text()').extract()
         genres = response.css(
             'div.details_block:nth-child(1) > a ::text').extract()
-        total_reviews = response.css(
-            'div.user_reviews_filter_menu:nth-child(1) > div:nth-child(2) > div:nth-child(1) > label:nth-child(2) > span:nth-child(1) ::text').extract_first()
-        positive_reviews = response.css(
-            'div.user_reviews_filter_menu:nth-child(1) > div:nth-child(2) > div:nth-child(1) > label:nth-child(5) > span:nth-child(1) ::text').extract_first()
-        negative_reviews = response.css(
-            'div.user_reviews_filter_menu:nth-child(1) > div:nth-child(2) > div:nth-child(1) > label:nth-child(8) > span:nth-child(1) ::text').extract_first()
-        english_reviews = response.css(
-            'div.user_reviews_filter_menu:nth-child(3) > div:nth-child(2) > div:nth-child(1) > label:nth-child(5) > span:nth-child(1) ::text').extract_first()
+
+        # total_reviews = response.css(
+        #     'div.user_reviews_filter_menu:nth-child(1) > div:nth-child(2) > div:nth-child(1) > label:nth-child(2) > span:nth-child(1) ::text').extract_first()
+        # positive_reviews = response.css(
+        #     'div.user_reviews_filter_menu:nth-child(1) > div:nth-child(2) > div:nth-child(1) > label:nth-child(5) > span:nth-child(1) ::text').extract_first()
+        # negative_reviews = response.css(
+        #     'div.user_reviews_filter_menu:nth-child(1) > div:nth-child(2) > div:nth-child(1) > label:nth-child(8) > span:nth-child(1) ::text').extract_first()
+        # english_reviews = response.css(
+        #     'div.user_reviews_filter_menu:nth-child(3) > div:nth-child(2) > div:nth-child(1) > label:nth-child(5) > span:nth-child(1) ::text').extract_first()
 
         early_access = response.css('.early_access_header')
         if early_access:
@@ -58,20 +63,26 @@ class GamesSpider(scrapy.Spider):
         for genre in genres:
             stripped_genres.append(genre.strip())
 
-        yield {
-            # TODO: add appid
-            'title': title,
-            'developer': developer,
-            'publisher': publisher,
-            'tags': stripped_tags,
-            'genres': stripped_genres,
-            'url': url,
-            'early_access': is_early_access,
-            'total_reviews': total_reviews,
-            'positive_reviews': positive_reviews,
-            'negative_reviews': negative_reviews,
-            'english_reviews': english_reviews,
-        }
+        bi['tags'] = stripped_tags
+        bi['genres'] = stripped_genres
+        bi['early_access'] = is_early_access
+
+        # yield {
+        #     'appid': appid,
+        #     'title': title,
+        #     'developer': developer,
+        #     'publisher': publisher,
+        #     'tags': stripped_tags,
+        #     'genres': stripped_genres,
+        #     'url': url,
+        #     'early_access': is_early_access,
+        #     'total_reviews': total_reviews,
+        #     'positive_reviews': positive_reviews,
+        #     'negative_reviews': negative_reviews,
+        #     'english_reviews': english_reviews,
+        # }
+
+        yield bi
 # links to next pages
 # https://store.steampowered.com/search/?sort_by=Released_DESC&sort_order=DESC&page=2
 # a.pagebtn <- extract all pagebtn and take the second one
